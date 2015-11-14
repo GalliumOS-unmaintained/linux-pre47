3d66e8dff78335b3e3a0d39ea2f741a3064fc052
Hugh Greenberg <hugegreenbug@gmail.com>
2015-11-13 22:58:23 -0700

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 19 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#19 SMP PREEMPT Wed Nov 4 21:13:11 MST 2015"
+#define LINUX_COMPILE_BY "hugh"
+#define LINUX_COMPILE_HOST "hugh-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.9.2 (Ubuntu 4.9.2-10ubuntu13) "
